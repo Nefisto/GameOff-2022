e46e03468a4ac399d7686e3957d301af11b030cb
Nefisto <joao.gavron@gmail.com>
2022-10-28 15:34:16 -0300

    Added placeholder to test build on itch

---
+++ Assets/_Project/Scripts/Mono behaviors/ToBeAnnounced.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ToBeAnnounced : MonoBehaviour
+{
+    public Transform folder;
+    public GameObject tbaLabel;
+
+    public void CreateLabel()
+    {
+        var instance = Instantiate(tbaLabel, folder, false);
+        ((RectTransform)instance.transform).anchoredPosition = RandomizePosition();
+    }
+
+    private Vector2 RandomizePosition() => new(Random.Range(-(1920 * .5f), (1920 * .5f)), Random.Range(-(1080f * .5f), (1080f * .5f)));
+}
