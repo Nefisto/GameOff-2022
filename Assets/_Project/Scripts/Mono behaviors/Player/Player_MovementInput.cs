03504f4fbccae31be60f442f59c30f12b0b14986
Nefisto <joao.gavron@gmail.com>
2022-11-24 16:31:34 -0300

    Updated character animator

--- Assets/_Project/Scripts/Mono behaviors/Player/Player_MovementInput.cs
+++ Assets/_Project/Scripts/Mono behaviors/Player/Player_MovementInput.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public partial class Player
 {
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+
     public bool CanMove()
         => currentDirection != Vector2.zero;
 
     public void Move (Vector2 direction)
-        => transform.Translate(direction * speed * Time.deltaTime);
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        SetAnimatorMovement(direction);
+        animator.Play("Player walk");
+    }
+
+    private void SetAnimatorMovement (Vector2 direction)
+    {
+        animator.SetFloat(Horizontal, direction.x);
+        animator.SetFloat(Vertical, direction.y);
+    }
 
     private void RegisterMovementInput()
     {
         GameInputAccessor.Gameplay.Movement.performed += ctx => currentDirection = ctx.ReadValue<Vector2>();
-        GameInputAccessor.Gameplay.Movement.canceled += _ => currentDirection = Vector2.zero;
+        GameInputAccessor.Gameplay.Movement.canceled += _ =>
+        {
+            animator.Play("Player idle");
+            currentDirection = Vector2.zero;
+        };
     }
 }
